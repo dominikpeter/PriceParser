331881c919608d2fa7648c042293a128279e271c
dominikpeter <dominik.peter87@gmail.com>
2017-11-14 13:15:08 +0100

    init

--- Analyser.py
+++ Analyser.py
@@ -15,7 +15,6 @@ import turbodbc
 
 import PriceParser as pp
 
-currenpath = os.getcwd()
 
 def load_file(filepath):
     df = pp.csv_to_pandas(filepath)
@@ -27,13 +26,15 @@ def modify_dataframe(df):
     ltm_cols = [i for i in df.columns if re.match('.+_LTM', i)]
     df[ltm_cols] = df[ltm_cols].astype(float)
 
-    preis_cols = [i for i in df.columns if re.match('Preis.*', i)]
+    preis_cols = [i for i in df.columns if re.match('Preis_.*', i)]
+    preis_cols = [i for i in preis_cols if i not in ['Preis_EAN']]
     df[preis_cols] = df[preis_cols].astype(float)
+    df['Preis'] = df['Preis'].astype(float)
     df['ObjectRate'] = df['ObjectRate'].astype(float)
 
     df['Erstellt_Am'] = pd.to_datetime(df['Erstellt_Am'])
-    df['Avg_Preis'] = np.mean(df[preis_cols], axis=1)
-    df['Std_Preis'] = np.std(df[preis_cols], axis=1)
+    df['Avg_Preis'] = df[preis_cols].mean(axis=1)
+    df['Std_Preis'] = df[preis_cols].std(axis=1)
     df['Neuer_Preis'] = np.nan
     df['Neuer_Faktor'] = np.nan
     df['Neue_Gruppe'] = np.nan
@@ -56,25 +57,56 @@ def select_df(df):
                 'Joined_TeamHug_on', 'Distance_TeamHug',
                 'Preis_TeamSaniDusch', 'Txt_Lang_TeamSaniDusch',
                 'Joined_TeamSaniDusch_on', 'Distance_TeamSaniDusch',
-                'GrossSales LTM','Sales_LTM', 'Margin_LTM',
+                'Preis_ArthurWeber','Txt_Lang_ArthurWeber',
+                'Joined_ArthurWeber_on',
+                'Distance_ArthurWeber','Preis_Briner','Txt_Lang_Briner',
+                'Joined_Briner_on','Distance_Briner',
+                'Preis_DebrunnerAciferTB',
+                'Txt_Lang_DebrunnerAciferTB','Joined_DebrunnerAciferTB_on',
+                'Distance_DebrunnerAciferTB',
+                'Preis_Engel','Txt_Lang_Engel','Joined_Engel_on',
+                'Distance_Engel','Preis_Gabs','Txt_Lang_Gabs',
+                'Joined_Gabs_on','Distance_Gabs','Preis_ISOCENTER',
+                'Txt_Lang_ISOCENTER','Joined_ISOCENTER_on',
+                'Distance_ISOCENTER',
+                'Preis_Nussbaum','Txt_Lang_Nussbaum','Joined_Nussbaum_on',
+                'Distance_Nussbaum','Preis_Pestalozzi','Txt_Lang_Pestalozzi',
+                'Joined_Pestalozzi_on','Distance_Pestalozzi',
+                'Preis_SchwarzStahl',
+                'Txt_Lang_SchwarzStahl','Joined_SchwarzStahl_on',
+                'Distance_SchwarzStahl',
+                'Preis_SpaeterHT','Txt_Lang_SpaeterHT','Joined_SpaeterHT_on',
+                'Distance_SpaeterHT','Preis_SpaeterHZ','Txt_Lang_SpaeterHZ',
+                'Joined_SpaeterHZ_on','Distance_SpaeterHZ','Preis_SpaeterWE',
+                'Txt_Lang_SpaeterWE','Joined_SpaeterWE_on',
+                'Distance_SpaeterWE',
+                'Preis_Tobler','Txt_Lang_Tobler','Joined_Tobler_on',
+                'Distance_Tobler','Preis_WalterMeier','Txt_Lang_WalterMeier',
+                'Joined_WalterMeier_on','Distance_WalterMeier',
+                'GrossSales_LTM','Sales_LTM', 'Margin_LTM',
                 'Quantity_LTM', 'ObjectRate',
                 'Lieferantenname', 'Erstellt_Am',
                 'Artikelserie', 'Wgr-Nr.',
                 'Warengruppebezeichnung', 'Preisbasis',
-                'Preisfaktor']
+                'Preisfaktor', 'Avg_Preis', 'Std_Preis',
+                'Neuer_Preis', 'Neuer_Faktor', 'Neue_Gruppe']
+
+    columns_ = [i for i in columns_ if i in df.columns]
+
     df = df[columns_]
 
     timediff_ = datetime.datetime.now() - df['Erstellt_Am']
 
-    select_  = (timediff_.apply(
-                    lambda x: x.days) < 180) | (
-                df['Quantity_LTM'] > 0.0)
+    select_ = (timediff_.apply(
+        lambda x: x.days) < 180) | (
+        df['Quantity_LTM'] > 0.0)
 
     pc_ = ~df['Art_Txt_Lang'].str.contains('ProCasa')
 
-    df_select = df.loc[select_ & pc_, :].copy().reset_index(drop=True)
+    df = df.loc[select_ & pc_, :].copy().reset_index(drop=True)
 
-    return df_select
+    return df
+
 
 def checks(df):
     mean_std_ = 1
@@ -84,65 +116,61 @@ def checks(df):
     top_sales_percentile_ = 95
     object_percentile_ = 40
 
-    c_avg = df_select['Preis'] < (df_select[
-            'Avg_Preis']-(df_select['Std_Preis'])*mean_std_)
+    c_avg = df['Preis'] < (df['Avg_Preis'] - (df['Std_Preis']) * mean_std_)
 
-    df_select['Check Avg'] = c_avg
-    df_select['Check_Sales'] = df_select[
-                                        'Sales_LTM'] < np.nanpercentile(
-                                            df_select['Sales_LTM'],
-                                                sales_percentile_)
+    df['Check Avg'] = c_avg
+    df['Check_Sales'] = df['Sales_LTM'] < np.nanpercentile(
+        df['Sales_LTM'],
+        sales_percentile_)
 
-    df_select['Check_Quantity'] = df_select[
-                                        'Quantity_LTM'] < np.nanpercentile(
-                                            df_select['Quantity_LTM'],
-                                                quantity_percentile_)
+    df['Check_Quantity'] = df[
+        'Quantity_LTM'] < np.nanpercentile(
+        df['Quantity_LTM'],
+        quantity_percentile_)
 
-    high_c = df_select[
-                'Quantity_LTM'] > np.nanpercentile(
-                                    df_select[
-                                        'Quantity_LTM'],
-                                            top_quantity_percentile_)
+    high_c = df['Quantity_LTM'] > np.nanpercentile(
+        df['Quantity_LTM'],
+        top_quantity_percentile_)
 
-    low_s = df_select['Sales_LTM'] < np.nanpercentile(
-                                            df_select[
-                                             'Sales_LTM'],
-                                                top_sales_percentile_)
+    low_s = df['Sales_LTM'] < np.nanpercentile(
+        df['Sales_LTM'],
+        top_sales_percentile_)
 
-    df_select[
-        'Check_High_Quantity_Low_Sales'] = high_c & low_s
+    df['Check_High_Quantity_Low_Sales'] = high_c & low_s
 
+    o_ = df['ObjectRate'] < np.nanpercentile(
+        df['ObjectRate'], object_percentile_)
 
-    o_ = df_select[
-            'ObjectRate'] < np.nanpercentile(
-                                df_select['ObjectRate'], object_percentile_)
+    df['Check_ObjectRate'] = o_
 
-    df_select['Check_ObjectRate'] = o_
+    return df
 
-    return df_select
 
 def write_df(df, output):
     print("Writing file...")
     df.to_csv(output, index=False, sep=';',
-                   encoding='utf-8', quoting=csv.QUOTE_NONNUMERIC)
+              encoding='utf-8', quoting=csv.QUOTE_NONNUMERIC)
 
 
 def main(input_, output_):
     df = load_file(input_)
     df = modify_dataframe(df)
     df_s = select_df(df)
+    df_s = checks(df_s)
+    df_s = df_s.drop_duplicates()
+
     write_df(df_s, output_)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Example with long option names')
-    parser.add_argument('--input', default="Price-Comparison.csv",
+        description='Analyser')
+    parser.add_argument('-i', default="Price-Comparison.csv",
                         dest="input",
-        help="Input File", type=str)
-    parser.add_argument('--output',
+                        help="Input File", type=str)
+    parser.add_argument('-o',
                         default="Price-Comparison-Analyser.csv", dest="output",
-        help="Output File", type=str)
+                        help="Output File", type=str)
 
     args = parser.parse_args()
 
--- Testing.py
+++ Testing.py
@@ -4,15 +4,38 @@ import pandas as pd
 import numpy as np
 import json
 import os
-
-p = pd.DataFrame({'A': [1,2,3,4,4,4,5,2,3,5,9,6,2,1,23, 23, 23 ,  23]})
-
-
-p['Count'] =  p.groupby('A')['A'].transform(lambda x: len(x))
-
-p[p.groupby('A')['A'].cumcount() == 0].index
+import random
+import PriceParser as pp
 
 
-a = ['A']
+def highlight_max(s):
+    '''
+    highlight the maximum in a Series yellow.
+    '''
+    try:
+        is_max = s == s.max()
+        return ['background-color: yellow' if v else '' for v in is_max]
+    except TypeError:
+        return ['background-color: white']
 
-p[a]
+def load_file(filepath):
+    df = pp.csv_to_pandas(filepath)
+
+    return df
+
+c = os.getcwd()
+path_ = os.path.join(c, "Matching", "2017-11-13_Price-Comparison-Sanitary-Analysis.csv")
+
+df = load_file(path_)
+
+def highlight_cols(x):
+    #copy df to new - original data are not changed
+    df = x.copy()
+    #select all values to default value - red color
+    df.loc[:,:] = 'background-color: white'
+    df.loc[df['Joined_Sanitas_on'] == 'Text Similarity','Preis_Sanitas'] = 'background-color: yellow'
+    #overwrite values grey color
+    #return color df
+    return df
+
+df.style.apply(highlight_cols, axis=None).to_excel('test.xlsx')
